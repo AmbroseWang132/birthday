a6b14b3fdb288841084d72109e9146b1b3692eb9
AmbroseWang132 <157398961+AmbroseWang132@users.noreply.github.com>
2024-12-02 21:35:53 -0500

    Update effect.js

--- effect.js
+++ effect.js
@@ -1,5 +1,5 @@
-$('#play').click(function(){
-		var audio = $('.song')[0];
-        audio.play();
-	$(this).hide();
-  });
+$('#play').click(function() {
+    var audio = $('.song')[0]; // Select the audio element
+    audio.play(); // Play the audio
+    $(this).hide(); // Hide the button
+});
