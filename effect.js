33f33cf4cd357fb167362beaed3eed004fc93451
AmbroseWang132 <157398961+AmbroseWang132@users.noreply.github.com>
2024-12-02 21:41:56 -0500

    Add files via upload

--- effect.js
+++ effect.js
@@ -1,5 +1,4 @@
-$('#play').click(function() {
-    var audio = $('.song')[0]; // Select the audio element
-    audio.play(); // Play the audio
-    $(this).hide(); // Hide the button
-});
+$('#play').click(function(){
+		var audio = $('.song')[0];
+        audio.play();
+  });
